a88a14a0b4ad4768c0f4ead98fce662a5a59884b
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2026-04-03 18:48:40 +0700

    Update PageNavigationBottomSheet.tsx

--- src/screens/novel/components/PageNavigationBottomSheet.tsx
+++ src/screens/novel/components/PageNavigationBottomSheet.tsx
@@ -62,7 +62,7 @@ export default function PageNavigationBottomSheet({
                 },
               ]}
             >
-              Page {item}
+              Volume {item}
             </Text>
           </View>
         </Pressable>
